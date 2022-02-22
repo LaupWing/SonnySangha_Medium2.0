a67fd762455896ac93ae59b43efbf5a6ab282942
LaupWing <loc_strict@hotmail.com>
2022-02-22 16:23:16 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
    return (
-      <div className="">
+      <div className='max-w-7xl mx-auto'>
          <Head>
             <title>Medium Blog</title>
             <link rel="icon" href="/favicon.ico" />
          </Head>
+         <Header/>
+         <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+            <div className='px-10 space-y-5'>
+               <h1 className='text-6xl font-serif max-w-xl'>
+                  <span className='underline decoration-black decoration-2'>Medium</span> is a place to write read and connect</h1>
+               <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti perspiciatis harum veniam.</h2>
+            </div>
 
+            <img 
+               className='hidden md:inline-flex lg:h-full h-32' 
+               src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" 
+               alt="" 
+            />
+         </div>
       </div>
    )
 }
